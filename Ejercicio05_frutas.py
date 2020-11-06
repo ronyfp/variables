c357f723c151f5e0ab23ae0e9c184e9f10854104
ronyfp <73153122+ronyfp@users.noreply.github.com>
2020-11-06 16:34:36 -0400

    Add files via upload

---
+++ Ejercicio05_frutas.py
@@ -0,0 +1,10 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> fruta1= 'naranja'
+>>> fruta2= 'pera'
+>>> fruta3= 'chinola'
+>>> fruta4= 'toronja'
+>>> fruta5= 'manzana'
+>>> print (fruta5)
+manzana
+>>> 
